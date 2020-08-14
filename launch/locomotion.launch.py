6d29447f6baa1883802c8f803b990e5c4f6dd585
Miro Voellmy <miro.voellmy@esa.int>
2020-08-14 16:12:32 +0200

    Remove namespace and output from locomotion launch file.

--- launch/locomotion.launch.py
+++ launch/locomotion.launch.py
@@ -4,7 +4,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument  # , SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 
@@ -14,12 +14,13 @@ from launch_ros.actions import Node
 
 from nav2_common.launch import RewrittenYaml
 
-# TODO: DO NOT USE THIS. Should be replaced w/ launch config.
-namespace_ = ''
-
 
 def generate_launch_description():
     """Launch description."""
+
+    # emulate_tty is needed to ouput the WARN, ERROR in colors.
+    emulate_tty = True
+
     # Get the directories
     rover_config_dir = get_package_share_directory('rover_config')
 
@@ -33,7 +34,6 @@ def generate_launch_description():
 
     # Create the launch configuration variables
     config_file = LaunchConfiguration('config_file')
-    namespace = LaunchConfiguration('namespace')
     robot_description = LaunchConfiguration('robot_description')
     urdf_path = LaunchConfiguration('urdf_path')
     use_sim_time = LaunchConfiguration('use_sim_time')
@@ -44,11 +44,6 @@ def generate_launch_description():
         'config_file',
         default_value=os.path.join(rover_config_dir, 'config', 'marta.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
-
-    declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace',
-        default_value='',
-        description='Top-level namespace')
 
     declare_urdf_path_cmd = DeclareLaunchArgument(
         'urdf_path',
@@ -79,14 +74,13 @@ def generate_launch_description():
 
     configured_params = RewrittenYaml(
         source_file=config_file,
-        root_key=namespace,
+        root_key='',
         param_rewrites=param_substitutions,
         convert_types=True)
 
     return LaunchDescription([
         # Launch Arguments
         declare_config_file_cmd,
-        declare_namespace_cmd,
         declare_urdf_path_cmd,
         declare_robot_description_cmd,
         declare_use_sim_time_cmd,
@@ -95,79 +89,61 @@ def generate_launch_description():
         # Nodes
         Node(
             package='robot_state_publisher',
-            namespace=namespace_,
             executable='robot_state_publisher',
             name='robot_state_publisher_node',
-            remappings=[
-                    ('/joint_states', os.path.join(namespace_, '/joint_states'))
-            ],
-            # arguments=[robot_description],
-            # TODO: Remove arguments once the robot_desc. works via parameters
-            #       Should be with Foxy
+            remappings=[('/joint_states', '/joint_states')],
             parameters=[configured_params],
-            emulate_tty=True
+            emulate_tty=emulate_tty
         ),
         Node(
             package='joy',
-            namespace=namespace_,
             executable='joy_node',
             name='joy_node',
             remappings=[
                     ('joy', 'gamepad')
             ],
-            output='screen',
             parameters=[configured_params],
-            emulate_tty=True
+            emulate_tty=emulate_tty
         ),
         Node(
             package='gamepad_parser',
-            namespace=namespace_,
             executable='gamepad_parser_node',
             name='gamepad_parser_node',
-            output='screen',
-            remappings=[(os.path.join(namespace_, '/rover_motion_cmd'), '/cmd_vel')],
+            remappings=[('/rover_motion_cmd', '/cmd_vel')],
             parameters=[configured_params],
-            emulate_tty=True
+            emulate_tty=emulate_tty
         ),
         Node(
             package='locomotion_manager',
-            namespace=namespace_,
             executable='locomotion_manager_node',
             name='locomotion_manager_node',
-            output='screen',
             parameters=[configured_params],
-            emulate_tty=True
+            emulate_tty=emulate_tty
         ),
         Node(
             package='simple_rover_locomotion',
-            namespace=namespace_,
             executable='simple_rover_locomotion_node',
             name='simple_rover_locomotion_node',
-            remappings=[(os.path.join(namespace_, '/rover_motion_cmd'), '/cmd_vel')],
-            output='screen',
-            emulate_tty=True,
+            remappings=[('/rover_motion_cmd', '/cmd_vel')],
+            emulate_tty=emulate_tty,
             # Parameters can be passed as dict or path to the .yaml
             parameters=[configured_params]
         ),
         Node(
             package='locomotion_mode',
-            namespace=namespace_,
             executable='stop_mode_node',
             name='stop_mode_node',
-            remappings=[(os.path.join(namespace_, '/rover_motion_cmd'), '/cmd_vel')],
-            output='screen',
-            emulate_tty=True,
+            remappings=[('/rover_motion_cmd', '/cmd_vel')],
+            emulate_tty=emulate_tty,
             # Parameters can be passed as dict or path to the .yaml
             parameters=[configured_params]
         ),
         Node(
             condition=IfCondition(use_ptu),
             package='ptu_control',
-            namespace=namespace_,
             executable='ptu_control_node',
             name='ptu_control_node',
-            output='screen',
-            emulate_tty=True,
+            emulate_tty=emulate_tty,
             parameters=[configured_params]
         )
     ])
