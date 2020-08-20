d47cb35518b5621ca9ba1a9dc830fd108d1c209f
Miro Voellmy <miro.voellmy@esa.int>
2020-08-20 15:58:02 +0200

    Add example GNSS driver launch file

---
+++ launch/nmea_serial_driver.launch.py
@@ -0,0 +1,35 @@
+# Copyright 2018 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" A simple launch file for the nmea_serial_driver node. """
+
+import os
+import sys
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    """Generate a launch description for a single serial driver."""
+    config_file = os.path.join(get_package_share_directory("rover_config"), "config", "nmea_serial_driver.yaml")
+    
+    driver_node = Node(
+        package='nmea_navsat_driver',
+        executable='nmea_serial_driver',
+        output='screen',
+        parameters=[config_file])
+
+    return LaunchDescription([driver_node])
