fb2f6a905ba7f7214db7fcd33d74c89a0b90b453
Diogo Brandao <diogo.pina.brandao@hotmail.com>
2020-12-02 15:59:35 +0100

    Fix launch argument default_bt_xml_filename
    - Path is now being created but marta is not moving when running
    navigation_simulation.launch.py

--- launch/nav2_navigation_launch.py
+++ launch/nav2_navigation_launch.py
@@ -32,7 +32,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     autostart = LaunchConfiguration('autostart')
     params_file = LaunchConfiguration('params_file')
-    bt_xml_file = LaunchConfiguration('bt_xml_file')
+    default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
     map_subscribe_transient_local = LaunchConfiguration('map_subscribe_transient_local')
 
     lifecycle_nodes = ['controller_server',
@@ -53,7 +53,7 @@ def generate_launch_description():
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
         'use_sim_time': use_sim_time,
-        'bt_xml_filename': bt_xml_file,
+        'default_bt_xml_filename': default_bt_xml_filename,
         'autostart': autostart,
         'map_subscribe_transient_local': map_subscribe_transient_local}
 
@@ -83,10 +83,10 @@ def generate_launch_description():
             description='Full path to the ROS2 parameters file to use'),
 
         DeclareLaunchArgument(
-            'bt_xml_file',
+            'default_bt_xml_filename',
             default_value=os.path.join(
                 get_package_share_directory('nav2_bt_navigator'),
-                'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
+                'behavior_trees', 'navigate_w_replanning_distance.xml'),
             description='Full path to the behavior tree xml file to use'),
 
         DeclareLaunchArgument(
